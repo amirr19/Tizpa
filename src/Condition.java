686cd407120f4d695998f48d795301629f35dbb4
sheikhi <hajisheikhi95@gmail.com>
2019-11-30 14:52:09 +0330

    check for update

--- src/Condition.java
+++ src/Condition.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class Condition {
+
+    static double[][] condictionCost =
+            {
+                    {
+                            1.4, 1.2, 1.2
+                    },
+                    {
+                            3, 2, 2
+                    },
+                    {
+                            1.5, 2, 0.8
+                    }
+            };
+
 }
+
